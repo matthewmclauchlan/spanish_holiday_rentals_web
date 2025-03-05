e1b22038f6c3e0d688df267ff81ad2d89da902cb
mattymclauchlan <info@spanishholidayrentals.co.uk>
2025-03-05 15:06:57 +0100

    fix build errors

--- app/(screens)/payment-cancel/page.tsx
+++ app/(screens)/payment-cancel/page.tsx
@@ -1,9 +1,11 @@
-// app/payment-cancel/page.tsx
+'use client';
+
+import React from 'react';
+
 export default function PaymentCancel() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-white">
-        <h1 className="text-3xl font-bold">Payment Canceled</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold">Payment Canceled</h1>
+    </div>
+  );
+}
